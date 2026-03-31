c4640aceebb0c4e67bd88835eaa2345a9b5a94b2
Hgyeol <neunimchi626@gmail.com>
2026-03-31 17:52:16 +0900

    한국 투자 api를 langchain tool로 추가

---
+++ tools/__init__.py
@@ -0,0 +1,324 @@
+import sys
+from pathlib import Path
+
+_tools_dir = Path(__file__).parent
+sys.path.insert(0, str(_tools_dir))
+sys.path.insert(0, str(_tools_dir.parent / 'examples_llm'))
+
+from after_hour_balance import after_hour_balance
+from asking_price_krx import asking_price_krx
+from asking_price_nxt import asking_price_nxt
+from asking_price_total import asking_price_total
+from bulk_trans_num import bulk_trans_num
+from capture_uplowprice import capture_uplowprice
+from ccnl_krx import ccnl_krx
+from ccnl_notice import ccnl_notice
+from ccnl_nxt import ccnl_nxt
+from ccnl_total import ccnl_total
+from chk_holiday import chk_holiday
+from comp_interest import comp_interest
+from comp_program_trade_daily import comp_program_trade_daily
+from comp_program_trade_today import comp_program_trade_today
+from credit_balance import credit_balance
+from credit_by_company import credit_by_company
+from daily_credit_balance import daily_credit_balance
+from daily_loan_trans import daily_loan_trans
+from daily_short_sale import daily_short_sale
+from disparity import disparity
+from dividend_rate import dividend_rate
+from estimate_perform import estimate_perform
+from exp_ccnl_krx import exp_ccnl_krx
+from exp_ccnl_nxt import exp_ccnl_nxt
+from exp_ccnl_total import exp_ccnl_total
+from exp_closing_price import exp_closing_price
+from exp_index_trend import exp_index_trend
+from exp_price_trend import exp_price_trend
+from exp_total_index import exp_total_index
+from exp_trans_updown import exp_trans_updown
+from finance_balance_sheet import finance_balance_sheet
+from finance_financial_ratio import finance_financial_ratio
+from finance_growth_ratio import finance_growth_ratio
+from finance_income_statement import finance_income_statement
+from finance_other_major_ratios import finance_other_major_ratios
+from finance_profit_ratio import finance_profit_ratio
+from finance_ratio import finance_ratio
+from finance_stability_ratio import finance_stability_ratio
+from fluctuation import fluctuation
+from foreign_institution_total import foreign_institution_total
+from frgnmem_pchs_trend import frgnmem_pchs_trend
+from frgnmem_trade_estimate import frgnmem_trade_estimate
+from frgnmem_trade_trend import frgnmem_trade_trend
+from hts_top_view import hts_top_view
+from index_ccnl import index_ccnl
+from index_exp_ccnl import index_exp_ccnl
+from index_program_trade import index_program_trade
+from inquire_account_balance import inquire_account_balance
+from inquire_asking_price_exp_ccn import inquire_asking_price_exp_ccn
+from inquire_balance import inquire_balance
+from inquire_balance_rlz_pl import inquire_balance_rlz_pl
+from inquire_ccnl import inquire_ccnl
+from inquire_credit_psamount import inquire_credit_psamount
+from inquire_daily_ccld import inquire_daily_ccld
+from inquire_daily_indexchartprice import inquire_daily_indexchartprice
+from inquire_daily_itemchartprice import inquire_daily_itemchartprice
+from inquire_daily_overtimeprice import inquire_daily_overtimeprice
+from inquire_daily_price import inquire_daily_price
+from inquire_daily_trade_volume import inquire_daily_trade_volume
+from inquire_elw_price import inquire_elw_price
+from inquire_index_category_price import inquire_index_category_price
+from inquire_index_daily_price import inquire_index_daily_price
+from inquire_index_price import inquire_index_price
+from inquire_index_tickprice import inquire_index_tickprice
+from inquire_index_timeprice import inquire_index_timeprice
+from inquire_investor import inquire_investor
+from inquire_investor_daily_by_market import inquire_investor_daily_by_market
+from inquire_investor_time_by_market import inquire_investor_time_by_market
+from inquire_member import inquire_member
+from inquire_member_daily import inquire_member_daily
+from inquire_overtime_asking_price import inquire_overtime_asking_price
+from inquire_overtime_price import inquire_overtime_price
+from inquire_period_profit import inquire_period_profit
+from inquire_period_trade_profit import inquire_period_trade_profit
+from inquire_price import inquire_price
+from inquire_price_2 import inquire_price_2
+from inquire_psbl_order import inquire_psbl_order
+from inquire_psbl_rvsecncl import inquire_psbl_rvsecncl
+from inquire_psbl_sell import inquire_psbl_sell
+from inquire_time_dailychartprice import inquire_time_dailychartprice
+from inquire_time_indexchartprice import inquire_time_indexchartprice
+from inquire_time_itemchartprice import inquire_time_itemchartprice
+from inquire_time_itemconclusion import inquire_time_itemconclusion
+from inquire_time_overtimeconclusion import inquire_time_overtimeconclusion
+from inquire_vi_status import inquire_vi_status
+from intgr_margin import intgr_margin
+from intstock_grouplist import intstock_grouplist
+from intstock_multprice import intstock_multprice
+from intstock_stocklist_by_group import intstock_stocklist_by_group
+from invest_opbysec import invest_opbysec
+from invest_opinion import invest_opinion
+from investor_program_trade_today import investor_program_trade_today
+from investor_trade_by_stock_daily import investor_trade_by_stock_daily
+from investor_trend_estimate import investor_trend_estimate
+from ksdinfo_bonus_issue import ksdinfo_bonus_issue
+from ksdinfo_cap_dcrs import ksdinfo_cap_dcrs
+from ksdinfo_dividend import ksdinfo_dividend
+from ksdinfo_forfeit import ksdinfo_forfeit
+from ksdinfo_list_info import ksdinfo_list_info
+from ksdinfo_mand_deposit import ksdinfo_mand_deposit
+from ksdinfo_merger_split import ksdinfo_merger_split
+from ksdinfo_paidin_capin import ksdinfo_paidin_capin
+from ksdinfo_pub_offer import ksdinfo_pub_offer
+from ksdinfo_purreq import ksdinfo_purreq
+from ksdinfo_rev_split import ksdinfo_rev_split
+from ksdinfo_sharehld_meet import ksdinfo_sharehld_meet
+from lendable_by_company import lendable_by_company
+from market_cap import market_cap
+from market_status_krx import market_status_krx
+from market_status_nxt import market_status_nxt
+from market_status_total import market_status_total
+from market_time import market_time
+from market_value import market_value
+from member_krx import member_krx
+from member_nxt import member_nxt
+from member_total import member_total
+from mktfunds import mktfunds
+from near_new_highlow import near_new_highlow
+from news_title import news_title
+from order_cash import order_cash
+from order_credit import order_credit
+from order_resv import order_resv
+from order_resv_ccnl import order_resv_ccnl
+from order_resv_rvsecncl import order_resv_rvsecncl
+from order_rvsecncl import order_rvsecncl
+from overtime_asking_price_krx import overtime_asking_price_krx
+from overtime_ccnl_krx import overtime_ccnl_krx
+from overtime_exp_ccnl_krx import overtime_exp_ccnl_krx
+from overtime_exp_trans_fluct import overtime_exp_trans_fluct
+from overtime_fluctuation import overtime_fluctuation
+from overtime_volume import overtime_volume
+from pbar_tratio import pbar_tratio
+from pension_inquire_balance import pension_inquire_balance
+from pension_inquire_daily_ccld import pension_inquire_daily_ccld
+from pension_inquire_deposit import pension_inquire_deposit
+from pension_inquire_present_balance import pension_inquire_present_balance
+from pension_inquire_psbl_order import pension_inquire_psbl_order
+from period_rights import period_rights
+from prefer_disparate_ratio import prefer_disparate_ratio
+from profit_asset_index import profit_asset_index
+from program_trade_by_stock import program_trade_by_stock
+from program_trade_by_stock_daily import program_trade_by_stock_daily
+from program_trade_krx import program_trade_krx
+from program_trade_nxt import program_trade_nxt
+from program_trade_total import program_trade_total
+from psearch_result import psearch_result
+from psearch_title import psearch_title
+from quote_balance import quote_balance
+from search_info import search_info
+from search_stock_info import search_stock_info
+from short_sale import short_sale
+from top_interest_stock import top_interest_stock
+from traded_by_company import traded_by_company
+from tradprt_byamt import tradprt_byamt
+from volume_power import volume_power
+from volume_rank import volume_rank
+
+all_ = [
+    after_hour_balance,
+    asking_price_krx,
+    asking_price_nxt,
+    asking_price_total,
+    bulk_trans_num,
+    capture_uplowprice,
+    ccnl_krx,
+    ccnl_notice,
+    ccnl_nxt,
+    ccnl_total,
+    chk_holiday,
+    comp_interest,
+    comp_program_trade_daily,
+    comp_program_trade_today,
+    credit_balance,
+    credit_by_company,
+    daily_credit_balance,
+    daily_loan_trans,
+    daily_short_sale,
+    disparity,
+    dividend_rate,
+    estimate_perform,
+    exp_ccnl_krx,
+    exp_ccnl_nxt,
+    exp_ccnl_total,
+    exp_closing_price,
+    exp_index_trend,
+    exp_price_trend,
+    exp_total_index,
+    exp_trans_updown,
+    finance_balance_sheet,
+    finance_financial_ratio,
+    finance_growth_ratio,
+    finance_income_statement,
+    finance_other_major_ratios,
+    finance_profit_ratio,
+    finance_ratio,
+    finance_stability_ratio,
+    fluctuation,
+    foreign_institution_total,
+    frgnmem_pchs_trend,
+    frgnmem_trade_estimate,
+    frgnmem_trade_trend,
+    hts_top_view,
+    index_ccnl,
+    index_exp_ccnl,
+    index_program_trade,
+    inquire_account_balance,
+    inquire_asking_price_exp_ccn,
+    inquire_balance,
+    inquire_balance_rlz_pl,
+    inquire_ccnl,
+    inquire_credit_psamount,
+    inquire_daily_ccld,
+    inquire_daily_indexchartprice,
+    inquire_daily_itemchartprice,
+    inquire_daily_overtimeprice,
+    inquire_daily_price,
+    inquire_daily_trade_volume,
+    inquire_elw_price,
+    inquire_index_category_price,
+    inquire_index_daily_price,
+    inquire_index_price,
+    inquire_index_tickprice,
+    inquire_index_timeprice,
+    inquire_investor,
+    inquire_investor_daily_by_market,
+    inquire_investor_time_by_market,
+    inquire_member,
+    inquire_member_daily,
+    inquire_overtime_asking_price,
+    inquire_overtime_price,
+    inquire_period_profit,
+    inquire_period_trade_profit,
+    inquire_price,
+    inquire_price_2,
+    inquire_psbl_order,
+    inquire_psbl_rvsecncl,
+    inquire_psbl_sell,
+    inquire_time_dailychartprice,
+    inquire_time_indexchartprice,
+    inquire_time_itemchartprice,
+    inquire_time_itemconclusion,
+    inquire_time_overtimeconclusion,
+    inquire_vi_status,
+    intgr_margin,
+    intstock_grouplist,
+    intstock_multprice,
+    intstock_stocklist_by_group,
+    invest_opbysec,
+    invest_opinion,
+    investor_program_trade_today,
+    investor_trade_by_stock_daily,
+    investor_trend_estimate,
+    ksdinfo_bonus_issue,
+    ksdinfo_cap_dcrs,
+    ksdinfo_dividend,
+    ksdinfo_forfeit,
+    ksdinfo_list_info,
+    ksdinfo_mand_deposit,
+    ksdinfo_merger_split,
+    ksdinfo_paidin_capin,
+    ksdinfo_pub_offer,
+    ksdinfo_purreq,
+    ksdinfo_rev_split,
+    ksdinfo_sharehld_meet,
+    lendable_by_company,
+    market_cap,
+    market_status_krx,
+    market_status_nxt,
+    market_status_total,
+    market_time,
+    market_value,
+    member_krx,
+    member_nxt,
+    member_total,
+    mktfunds,
+    near_new_highlow,
+    news_title,
+    order_cash,
+    order_credit,
+    order_resv,
+    order_resv_ccnl,
+    order_resv_rvsecncl,
+    order_rvsecncl,
+    overtime_asking_price_krx,
+    overtime_ccnl_krx,
+    overtime_exp_ccnl_krx,
+    overtime_exp_trans_fluct,
+    overtime_fluctuation,
+    overtime_volume,
+    pbar_tratio,
+    pension_inquire_balance,
+    pension_inquire_daily_ccld,
+    pension_inquire_deposit,
+    pension_inquire_present_balance,
+    pension_inquire_psbl_order,
+    period_rights,
+    prefer_disparate_ratio,
+    profit_asset_index,
+    program_trade_by_stock,
+    program_trade_by_stock_daily,
+    program_trade_krx,
+    program_trade_nxt,
+    program_trade_total,
+    psearch_result,
+    psearch_title,
+    quote_balance,
+    search_info,
+    search_stock_info,
+    short_sale,
+    top_interest_stock,
+    traded_by_company,
+    tradprt_byamt,
+    volume_power,
+    volume_rank,
+]
+
+print(len(all_))
